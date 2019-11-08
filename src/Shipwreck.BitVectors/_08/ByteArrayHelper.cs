15caaf214c3a247c44f5464083a4383c4c02f6a4
pgrho <shipwreck@live.jp>
2019-11-08 17:45:12 +0900

    Improved ByteArrayHelper.GetByteCore<T>

--- src/Shipwreck.BitVectors/_08/ByteArrayHelper.cs
+++ src/Shipwreck.BitVectors/_08/ByteArrayHelper.cs
@@ -4,6 +4,23 @@ namespace Shipwreck.BitVectors
 {
     internal static class ByteArrayHelper
     {
+        private static byte GetByteMask(int bits)
+        {
+            switch (bits)
+            {
+                case 0: return 0b0000_0000;
+                case 1: return 0b0000_0001;
+                case 2: return 0b0000_0011;
+                case 3: return 0b0000_0111;
+                case 4: return 0b0000_1111;
+                case 5: return 0b0001_1111;
+                case 6: return 0b0011_1111;
+                case 7: return 0b0111_1111;
+                case 8: return 0b1111_1111;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
         public static bool GetBitCore<T>(this T array, int index)
             where T : IByteArray
         {
@@ -22,7 +39,6 @@ namespace Shipwreck.BitVectors
             return ((array.GetElement(ei) >> (7 - (index & 7))) & 1) == 1;
         }
 
-        // TODO: optimize bit operations
         public static byte GetByteCore<T>(this T array, int startIndex, int length)
             where T : IByteArray
         {
@@ -30,18 +46,22 @@ namespace Shipwreck.BitVectors
             {
                 throw new ArgumentOutOfRangeException(nameof(startIndex));
             }
-            if (length < 0 || sizeof(byte) * 8 < length)
+            if (length == 0)
+            {
+                return 0;
+            }
+            const int SIZE = sizeof(byte) * 8;
+            if (length < 0 || SIZE < length)
             {
                 throw new ArgumentOutOfRangeException(nameof(length));
             }
 
-            int r = 0;
-            for (var i = 0; i < length; i++)
-            {
-                r |= array.GetBitCore(startIndex + i) ? 1 << (length - 1 - i) : 0;
-            }
+            var firstElement = startIndex / SIZE;
+            var firstBit = startIndex % SIZE;
+            var nextBit = firstBit + length;
 
-            return unchecked((byte)r);
+            var coreValue = nextBit > SIZE ? (array.GetElement(firstElement) << SIZE) + array.GetElement(firstElement + 1) : array.GetElement(firstElement);
+            return unchecked((byte)(GetByteMask(length) & (coreValue >> ((SIZE * 2 - nextBit) % SIZE))));
         }
 
         // TODO: optimize bit operations
