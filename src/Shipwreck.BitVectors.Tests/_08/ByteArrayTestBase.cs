61ff16352f527301911450cb838e6c3a3619d64b
pgrho <shipwreck@live.jp>
2019-11-08 17:12:20 +0900

    Added ByteArrayTestBase.GetByteTest

--- src/Shipwreck.BitVectors.Tests/_08/ByteArrayTestBase.cs
+++ src/Shipwreck.BitVectors.Tests/_08/ByteArrayTestBase.cs
@@ -42,5 +42,35 @@ namespace Shipwreck.BitVectors
                 Release();
             }
         }
+
+        internal sealed class GetByteTestDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+            {
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 0, 6, 0 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 0, 8, 0b10 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 0, 7, 1 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 6, 1, 1 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 6, 2, 0b10 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 6, 4, 0b1011 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 6, 8, 0b1011_0011 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 7, 8, 0b0110_0111 };
+                yield return new object[] { new byte[] { 0b0000_0010, 0b1100_1110 }, 8, 8, 0b1100_1110 };
+            }
+        }
+
+        [Theory]
+        [GetByteTestData]
+        public void GetByteTest(byte[] data, int startIndex, int length, byte expected)
+        {
+            try
+            {
+                Assert.Equal(expected, GetArray(data).GetByte(startIndex, length));
+            }
+            finally
+            {
+                Release();
+            }
+        }
     }
 }
